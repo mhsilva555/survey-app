ca980cd88fb72c07d542bc4a9d284ce32faa72ea
Marcos Henrique <mhsilva555@gmail.com>
2023-01-20 23:03:58 -0300

    Update order answer by total votes

--- app/Surveys/GetSurveyByID.php
+++ app/Surveys/GetSurveyByID.php
@@ -11,7 +11,7 @@ class GetSurveyByID
         $answers = $wpdb->prefix.ANSWERS;
 
         $survey = $wpdb->get_results("SELECT * FROM {$surveys} AS s INNER JOIN {$answers} AS a
-        ON s.survey_id = a.answer_survey_id WHERE survey_id = '{$survey_id}'", OBJECT);
+        ON s.survey_id = a.answer_survey_id WHERE survey_id = '{$survey_id}' ORDER BY answer_totalvotes DESC", OBJECT);
 
         return $survey;
     }
